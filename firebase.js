5755d3639b1a720930e7615557f3650e2c4cb857
Abdul Majid Hashmi <abdulmajidhashmi@Abduls-MacBook-Air.local>
2025-03-04 23:17:20 +0530

    check4

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth
 
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId:process.env.measurementId
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId,
+    measurementId:process.env.NEXT_PUBLIC_measurementId
   };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
